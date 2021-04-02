b96dc6254213b510ede359219f3b89c313434a2d
Patrick Creagan <pcreagan@wetdesign.com>
2021-04-01 23:31:22 -0700

    stations' charts local

--- src/stations/lighting/station2/LL2/view.py
+++
@@ -1,9 +0,0 @@
-from lighting.view import LightingView
-
-__all__ = [
-    'View',
-]
-
-
-class View(LightingView):
-    pass
--- src/stations/lighting/station3/chart/__init__.py
+++
@@ -1,3 +0,0 @@
-from lighting.LL3.chart.chart import *
-from lighting.LL3.chart.messages import *
-from lighting.LL3.chart.data import *
